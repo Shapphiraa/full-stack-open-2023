057123987f693fb9101d61a7b5ae8283b07b7125
Aida Martin <aidamg93@gmail.com>
2023-12-11 15:29:41 +0100

    Complete exercises to part 1 / unicafe 1.9 and 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,21 +2,18 @@ import { useState } from 'react'
 
 const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
 
-const Total = (props) => <p>{props.name} {props.total}</p>
+const StatisticLine = (props) => <p>{props.name} {props.value}</p>
 
 const Title = (props) => <h1 className="text-lg font-semibold">{props.title}</h1>
 
-const Extra = (props) => <p>{props.name} {props.calculation}</p>
-
 const Statistics = (props) => (
   <div>
-  <Title title="statistics"/>
-  <Total name="good" total={props.good}/>
-  <Total name="neutral" total={props.neutral}/>
-  <Total name="bad" total={props.bad}/>
-  <Extra name="all" calculation={props.good + props.neutral + props.bad}/>
-  <Extra name="average" calculation={(props.good - props.bad) / (props.good + props.neutral + props.bad)}/>
-  <Extra name="positive" calculation={props.good / (props.good + props.neutral + props.bad) * 100 + "%"}/>
+  <StatisticLine name="good" value={props.good}/>
+  <StatisticLine name="neutral" value={props.neutral}/>
+  <StatisticLine name="bad" value={props.bad}/>
+  <StatisticLine name="all" value={props.allClicks}/>
+  <StatisticLine name="average" value={(props.good - props.bad) / props.allClicks}/>
+  <StatisticLine name="positive" value={props.good / props.allClicks * 100 + "%"}/>
   </div>
 )
 
@@ -24,15 +21,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAllClicks] = useState(0)
 
   return (
     <div>
     <Title title="give feedback"/>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => {
+        setGood(good + 1)
+        setAllClicks(allClicks + 1)}
+        } text="good" />
+      <Button handleClick={() => {
+        setNeutral(neutral + 1)
+        setAllClicks(allClicks + 1)}
+      } text="neutral" />
+      <Button handleClick={() => {
+        setBad(bad + 1)
+        setAllClicks(allClicks + 1)}
+      } text="bad" />
 
-    <Statistics good={good} neutral={neutral} bad={bad}/>
+
+    <Title title="statistics"/>
+    {allClicks ? <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks}/> : "No feedback given"}
+    
     </div>
   )
 }
