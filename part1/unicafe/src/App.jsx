dd57d0258e0011a8dd4e48d8a0fb4c0e402a5d57
Aida Martin <aidamg93@gmail.com>
2023-12-11 12:55:51 +0100

    Complete exercise to part 1 / unicafe 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+const Total = (props) => <p>{props.name} {props.total}</p>
+
+const Title = (props) => <h1 className="text-lg font-semibold">{props.title}</h1>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+    <Title title="give feedback"/>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+    <Title title="statistics"/>
+    <Total name="good" total={good}/>
+    <Total name="neutral" total={neutral}/>
+    <Total name="bad" total={bad}/>
+    </div>
+  )
+}
+
+export default App
