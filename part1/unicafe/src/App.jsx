a8f7ac3df16411972962efab0bcc0450303514b7
Aida Martin <aidamg93@gmail.com>
2023-12-11 15:16:39 +0100

    Complete exercise to part 1 / unicafe 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,20 @@ const Total = (props) => <p>{props.name} {props.total}</p>
 
 const Title = (props) => <h1 className="text-lg font-semibold">{props.title}</h1>
 
+const Extra = (props) => <p>{props.name} {props.calculation}</p>
+
+const Statistics = (props) => (
+  <div>
+  <Title title="statistics"/>
+  <Total name="good" total={props.good}/>
+  <Total name="neutral" total={props.neutral}/>
+  <Total name="bad" total={props.bad}/>
+  <Extra name="all" calculation={props.good + props.neutral + props.bad}/>
+  <Extra name="average" calculation={(props.good - props.bad) / (props.good + props.neutral + props.bad)}/>
+  <Extra name="positive" calculation={props.good / (props.good + props.neutral + props.bad) * 100 + "%"}/>
+  </div>
+)
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -18,13 +32,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
-    <Title title="statistics"/>
-    <Total name="good" total={good}/>
-    <Total name="neutral" total={neutral}/>
-    <Total name="bad" total={bad}/>
-    <p>all {good + neutral + bad}</p>
-    <p>average {(good - bad) / (good + neutral + bad)}</p>
-    <p>positive {good / (good + neutral + bad) * 100}%</p>
+    <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
